fe7a6a939274011694fe1923b649aa32c7cf5a93
Harikrishnan D <harideegee@gmail.com>
2023-06-07 18:36:41 +0530

    edited archive naming system

--- 6-file compression/zipcreator.py
+++ 6-file compression/zipcreator.py
@@ -1,15 +1,12 @@
 import zipfile
 import pathlib
+import time
 
 def Zip(filepaths, destination):
 
     """ Creates an archive of the file(s) provided as arguments to 'filepaths' """
 
-    if len(filepaths) == 1:
-        destination_path = pathlib.Path(destination, "compressed_file.zip")
-    else:
-        destination_path = pathlib.Path(destination, "compressed_files.zip")
-    
+    destination_path = pathlib.Path(destination, f"compressed-{time.strftime('%d-%b-%Y-%I-%M-%S-%p')}.zip")
     with zipfile.ZipFile(destination_path, "w") as archive:
         for file in filepaths:
             file = pathlib.Path(file)
